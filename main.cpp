d6220a4053b5fe5d971d69a34ce8a20e4664b83c
Twan <twan.hoogveld@student.hu.nl>
2021-01-06 15:10:10 +0100

    update

--- main.cpp
+++ main.cpp
@@ -56,7 +56,7 @@ int main()
     std::vector<sf::RectangleShape> colors = {redBox,greenBox,blueBox,yellowBox,whiteBox,purpleBox,cyanBox};
     //---------------------------------------------------------------------------
     sf::Image img;
-    img.loadFromFile("move.png");
+    img.loadFromFile("arrows.png");
     sf::Texture texture;
     texture.loadFromImage(img);
     sf::Sprite sprite(texture);
