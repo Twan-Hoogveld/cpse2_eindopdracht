e8945a765080aa5dbda2ac415fd6a3682b92a4a1
Twan <twan.hoogveld@student.hu.nl>
2021-01-10 16:56:25 +0100

    update

--- Storage.hpp
+++ Storage.hpp
@@ -63,6 +63,7 @@ class Storage {
     //to swap moet vooraan in de lijst komen te staan. dus eerst de index ervan zoeken.
     int index = -1;
     for(unsigned int i = 0; i < count; i++){ if ( store[i] == to_swap) { index = i; break; }} //check using mem adress
+    if (index == -1) { std::cout << "i not found." << std::endl; break;}
     std::cout << "found i" << std::endl;
     //Index is nu bekend. dus nu index swappen met 0;
     T temp = store[0];
